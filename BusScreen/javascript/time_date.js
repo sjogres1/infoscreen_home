5887fa2d6c5cc4fde8953fca2dc7c8fc20a84a85
Sauli Sjögren <sjogres1@aalto.fi>
2018-05-28 17:21:28 +0300

    fixed temperature floating number bug

--- BusScreen/javascript/time_date.js
+++ BusScreen/javascript/time_date.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
     function getTimeDate() {
         // Show current time and date. Clock on the screen
         date = new Date();
-	yy = date.getYear();
-	year = (yy < 1000) ? yy + 1900 : yy;
-	$('#date').html(date.getDate() + "." + (date.getMonth() + 1) + "." + year);
+        yy = date.getYear();
+        year = (yy < 1000) ? yy + 1900 : yy;
+        $('#date').html(date.getDate() + "." + (date.getMonth() + 1) + "." + year);
         $('.hour').html(addZero(date.getHours()) + ":");
         $('.min').html(addZero(date.getMinutes()) + ":");
         $('.second').html(addZero(date.getSeconds()));
@@ -23,16 +23,18 @@ $(document).ready(function () {
     }
     window.setInterval(getTimeDate, 1000);
 
- function print_temperature() {
-           $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=6295613&mode=json&APPID=f2504fd23a51d501c64e5fa91c4f8f84", function (data) {
-        var temp_celcius = data.main.temp - 273.15;
-        $("#temperature").html(temp_celcius + "&#x2103");
-               console.log(data.wind.speed);
-    });
+    function print_temperature() {
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=6295613&mode=json&APPID=f2504fd23a51d501c64e5fa91c4f8f84", function (data) {
+            var temp_celcius = data.main.temp - 273.15;
+            var temp = temp_celcius.toFixed(1);
+            temp_celcius = temp;
+            $("#temperature").html(temp_celcius + "&#x2103");
+            console.log(data.wind.speed);
+        });
         setTimeout(function () {
             print_temperature();
         }, 120000);
-     
+
     }
     print_temperature();
 });
