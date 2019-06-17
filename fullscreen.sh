551c231150d1eebe69cd8857354cb0f01935b817
sjogres1 <sauli.sjogren@aalto.fi>
2019-06-17 21:43:58 +0300

    changed iceweasel to chromium as it is better supported

--- fullscreen.sh
+++ fullscreen.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-sudo rm -r ~/.cache/mozilla/firefox/*.default/*
-iceweasel localhost --display=:0 &
+sudo rm -r ~/.cache/chromium/Default/Cache/*
+chromium-browser localhost --display=:0 &
 sleep 20s;
 xte "key F11" -x:0
